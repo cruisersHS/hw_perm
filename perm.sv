aa6c0c6f3430d15d5d068bd06c03b9ed307bd337
cruisers <89080385@qq.com>
2020-10-29 17:04:58 -0700

    IDLE

--- perm.sv
+++ perm.sv
@@ -24,8 +24,10 @@ module perm_blk(input clk, input rst, input pushin, output reg stopin,
 	input [63:0] m4rd,
 	output reg [2:0] m4wx, output reg [2:0] m4wy,output reg m4wr,
 	output reg [63:0] m4wd,
-	output reg pushout, output reg firstout, output reg [63:0] dout
+	output reg pushout, input stopout, output reg firstout, output reg [63:0] dout
 	);
+	
+	reg stopin_d;
 	
 	enum [3:0] {
 	IDLE,
@@ -39,46 +41,114 @@ module perm_blk(input clk, input rst, input pushin, output reg stopin,
 	} cs, ns;
 	
 	reg [2:0] x, y, cx, cy;
+	reg write_rdy, write_rdy_d;		//write ready
 	
 	//pushin: data input to m1
 	//stopin: x4y4 stop input
 	
 	//state logic
-	always_ff @(posedge clk) begin
-		ns <= cs;
+	always_comb @(posedge clk) begin
+		ns = cs;
 		case(cs)
 			IDLE: begin
-				$display("in IDLE %t", $time);
-				#100 $finish;
+				if(write_rdy) begin
+					ns = INPUT;
+				end else begin
+					ns = IDLE;
+				end
 			end
 			
+			INPUT: begin
+				if(cx == 4 && cy == 4) begin
+					//ns = THETA_1
+					$finish;
+				end else begin
+					ns = INPUT;
+				end
+			end
 			
 			default: begin
 			end
 		endcase
 	end
 	
+	//data
+	always_comb @(*) begin
+		
+		case(cs)
+			IDLE: begin
+				$display("\nIDLE\n, %t", $time);
+			end
+			
+			INPUT: begin
+				$display("INPUT,x%dy%d,din=%h, %t", x, y, din, $time);
+				m1wx = x;
+				m1wy = y;
+			end
+			
+		endcase
+	end
 	
+	//cx, cy
+	always_ff @(posedge clk) begin
+		cx <= #1 x;
+		cy <= #1 y;
+		m1wr <= #1 0;
+		case(cs)
+			IDLE: begin
+			end
+			
+			INPUT: begin
+				$display("cx = %d, cy = %d, %t", cx, cy, $time);
+				if(cx == 4 && cy == 4) begin
+					cx <= #1 0;
+					cy <= #1 0;
+					m1wr <= #1 1;
+				end else if (cy == 4) begin
+					cx <= #1 x + 1;
+					cy <= #1 0;
+					m1wr <= #1 1;
+				end else begin
+					cx <= #1 x + 1;
+					cy <= #1 y + 1;
+					m1wr <= #1 1;
+				end
+			end
+			
+		endcase
+	end
 	
-	
+	//rdy
+	always_ff @(posedge pushin) begin
+		//write_rdy_d = #1 write_rdy;
+		if(!stopin) begin
+			write_rdy_d <= #1 1;
+		end else begin
+			write_rdy_d <= #1 0;
+		end
+	end
 	
 	//cs, ns
-	always_ff @(posedge clk) begin
+	always_ff @(posedge clk or poesdge rst) begin
 		if(rst) begin
-			cs <= 0;
+			cs <=#1 0;
 		end else begin
-			cs <= ns;
+			cs <=#1 ns;
 		end
 	end
 	
 	//ds
-	always_ff @(posedge clk) begin
+	always_ff @(posedge clk or poesdge rst) begin
 		if(rst) begin
-			x <=#1 0;
-			y <=#1 0;
+			x <= #1 0;
+			y <= #1 0;
+			stopin <= #1 0;
+			write_rdy <= #1 0;
 		end else begin
-			x <=#1 cx;
-			y <=#1 cy;
+			x <= #1 cx;
+			y <= #1 cy;
+			stopin <= #1 stopin_d;
+			write_rdy <= #1 write_rdy_d;
 		end
 	end
 	
